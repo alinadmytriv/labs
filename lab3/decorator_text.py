c9e64f19ac5734cd8c3e9f77ff6bdc202fd5a1d4
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-10-10 14:33:26 +0300

    Update decorator_text.py

--- lab3/decorator_text.py
+++ lab3/decorator_text.py
@@ -1,16 +1,22 @@
-def html_decorator(func):
-    def wrap(*args):
-        s = func(*args)
-        print("<" + s[0] + "> " + s[1] + " </" + s[0] + ">")
-    return wrap
+# decorator text with html tags
+def html_tag(tag=""):
+    def html_decorator(func):
+        def wrap(text1):
+            print("<" + tag + "> " + func(text1) + " </" + tag + ">")
+        return wrap
+    return html_decorator
 
+# function for decorator
+@html_tag("p")
+def html_string(text0):
+    return text0
 
-@html_decorator
-def html_string(*args):
-    return args
+# a main function which ask to input text
+# which will be inside the html tags
+def main():
+    s = input("Enter text fot html tags: ")
+    return html_string(s)
 
-
-t = input("Enter html tag: ")
-s = input("Enter text fot html tags: ")
-html_string(t, s)
-
+# call the main function
+if __name__ == '__main__':
+    main()
