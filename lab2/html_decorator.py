046835454447e8222e7471c74ee2822405c6eec4
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-10-10 14:28:44 +0300

    Update html_decorator.py

--- lab2/html_decorator.py
+++ lab2/html_decorator.py
@@ -1,55 +1,48 @@
 # decorator for escaping html tags
 def decor_html(func):
     def read_file(str0):
-        s = func(str0)
         # create a file that will include text with escaped html tags
         new_file = open("new-html-file.txt", "w")
-        for x in s:
-            if x == "<":
-                x = "&lt;"
-                new_file.write(x)
-            elif x == ">":
-                x = "&gt;"
-                new_file.write(x)
-            elif x == "&":
-                x = "&amp;"
-                new_file.write(x)
-            elif x == "\"":
-                x = "&quot;"
+        # create a file that will include text with escaped html tags
+        tags = {"<": "&lt;", ">": "&gt;", "&": "&amp;", "\"": "&quot;"}
+        for x in func(str0):
+            if x in tags.keys():
+                x = tags[x]
                 new_file.write(x)
             else:
                 new_file.write(x)
         new_file.close()
         # read the created file
-        new_file = open('new-html-file.txt', "r")
-        print(new_file.read())
-        new_file.close()
+        with open("new-html-file.txt") as nf:
+            print(nf.read())
     return read_file
 
 
 # function for decorator
 @decor_html
 def open_file_for_escape(string):
-    file = open(string, "r")
-    text = file.read()
-    file.close()
+    with open(string) as f:
+        text = f.read()
     return text
-
 
 # function which just open and read the file
 def open_file(string):
-    file = open(string, "r")
-    text = file.read()
-    file.close()
+    with open(string) as f:
+        text = f.read()
     return print(text)
 
+# the main function
+def main():
+    a = input("Do you want to escape html tags(escape) or read the file(read) or exit(exit)? - ")
+    if a == "escape":
+        file3 = input("Write the file name: ")
+        open_file_for_escape(file3)
+    elif a == "read":
+        file3 = input("Write the file name: ")
+        open_file(file3)
+    else:
+        print("Bye-Bye")
 
-a = input("Do you want to escape html tags(escape) or read the file(read) or exit(exit)? - ")
-if a == "escape":
-    file3 = input("Write the file name: ")
-    open_file_for_escape(file3)
-elif a == "read":
-    file3 = input("Write the file name: ")
-    open_file(file3)
-else:
-    print("Bye-Bye")
+# call the main function
+if __name__ == '__main__':
+    main()
