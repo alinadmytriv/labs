d7dd9bf0a21cfb7e6e370ba1e9dbd9e4714e1c92
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-11-08 21:09:29 +0200

    update lab7. download files

--- lab7/download_files.py
+++ lab7/download_files.py
@@ -2,32 +2,29 @@ from urllib import request
 
 def download_url_file(url0):
     file_open = request.urlopen(url0)
-    file_info = file_open.read().decode('utf-8')
-    file_info_str = str(file_info)
-    file_lines = file_info_str.split("\\n")
-
-    new_file = open("file.txt", "w")
-    for info in file_lines:
-        new_file.write(info + "\n")
-    new_file.close()
-    return new_file.name
+    chunk_size = 1024 * 10
+    while 1:
+        file_info = str(file_open.read(chunk_size).decode('utf-8')).split("\n")
+        if not file_info:
+            break
+        return file_info
 
 def words_count(file):
     dict = {}
     count = 0
-    with open(file) as f:
-        for line in f:
-            # remove the leading spaces and newlines characters
-            # convert the characters in line to lowercase to avoid case mismatch
-            # split the line into words
-            line = line.strip().lower().split(" ")
-            for word in line:
-                if word in dict:
-                    # increment count of word by 1
-                    dict[word] += 1
-                else:
-                    # add the word to dictionary with count 1
-                    dict[word] = 1
+    for line in file:
+        # remove the leading spaces and newlines characters
+        # convert the characters in line to lowercase to avoid case mismatch
+        # split the line into words
+        line = line.strip().lower().split(" ")
+        for word in line:
+            if word in dict:
+                # increment count of word by 1
+                dict[word] += 1
+            else:
+                # add the word to dictionary with count 1
+                dict[word] = 1
+    print("Статистика слів:")
     for y in dict.keys():
         print(f"{y} : {dict[y]}")
         count += dict[y]
@@ -36,8 +33,7 @@ def words_count(file):
 
 def main():
     url = input("Enter the url file: ")
-    download_url_file(url)
-    print(words_count(download_url_file(url)))
+    print(f"Кількість слів - {words_count(download_url_file(url))}")
 
 if __name__ == '__main__':
     main()
