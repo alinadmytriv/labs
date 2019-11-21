80ae4b77c1ec4a6efcf0c941927d41b0511c41f8
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-11-21 23:32:53 +0200

    update lab7. download files

--- lab7/download_files.py
+++ lab7/download_files.py
@@ -3,7 +3,7 @@ from urllib import request
 def download_url_file(url0):
     file_open = request.urlopen(url0)
     chunk_size = 1024 * 10
-    while 1:
+    while True:
         file_info = str(file_open.read(chunk_size).decode('utf-8')).split("\n")
         if not file_info:
             break
@@ -13,14 +13,13 @@ def words_stats(file):
     dictionary = {}
     count = 0
     for line in file:
-        line = line.strip().lower().split(" ")
-        for word in line:
+        # line = line.strip().lower().split(" ")
+        for word in line.strip().lower().split(" "):
             if word in dictionary:
                 dictionary[word] += 1
-                count += 1
             else:
                 dictionary[word] = 1
-                count += 1
+            count += 1
     return dictionary, count
 
 def main():
