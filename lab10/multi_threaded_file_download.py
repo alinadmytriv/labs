0037e0c400895743a7989cb2ee0f53dd3870207a
lsbmy <50499203+lsbmy@users.noreply.github.com>
2019-11-28 15:14:51 +0200

    update lab10. multi-threaded file download

--- lab10/multi_threaded_file_download.py
+++ lab10/multi_threaded_file_download.py
@@ -2,7 +2,7 @@ import os
 from urllib import request
 from threading import Thread
 
-class download_url_files(Thread):
+class download_url_files1(Thread):
     def __init__(self, url, number):
         """ the thread's initialization """
         Thread.__init__(self)
@@ -22,16 +22,55 @@ class download_url_files(Thread):
                 handler.write(file_info)
         print(f"Thread {self.number} finished to download {self.url}")
 
+class download_url_files2(Thread):
+    def __init__(self, url, number):
+        """ the thread's initialization """
+        Thread.__init__(self)
+        self.number = number
+        self.url = url
+
+    def run(self):
+        """ start the thread """
+        file_open = request.urlopen(self.url)
+        file_name = os.path.basename(self.url)
+        chunk_size = 1024 * 2
+        with open(file_name, "wb") as handler:
+            while True:
+                file_info = file_open.read(chunk_size)
+                if not file_info:
+                    break
+                handler.write(file_info)
+        print(f"Thread {self.number} finished to download {self.url}")
+
+class download_url_files3(Thread):
+    def __init__(self, url, number):
+        """ the thread's initialization """
+        Thread.__init__(self)
+        self.number = number
+        self.url = url
+
+    def run(self):
+        """ start the thread """
+        file_open = request.urlopen(self.url)
+        file_name = os.path.basename(self.url)
+        chunk_size = 1024 * 2
+        with open(file_name, "wb") as handler:
+            while True:
+                file_info = file_open.read(chunk_size)
+                if not file_info:
+                    break
+                handler.write(file_info)
+        print(f"Thread {self.number} finished to download {self.url}")
 
 def main():
     """ run the program """
-    urls = ["http://www.mathros.net.ua/rozvjazuvannja-zvychajnyh-dyferencialnyh-rivnjan-metodom-adamsa.html",
-            "http://thefuture.news/camera360",
-            "http://thefuture.news/blog/futurio-app-ar-lesson"]
-    for i, url in enumerate(urls):
-        thread = download_url_files(url, i+1)
-        thread.start()
-
+    urls = "http://www.mathros.net.ua/rozvjazuvannja-zvychajnyh-dyferencialnyh-rivnjan-metodom-adamsa.html"
+    thread1 = download_url_files1(urls, 1)
+    thread1.start()
+    thread2 = download_url_files2(urls, 2)
+    thread2.start()
+    thread3 = download_url_files3(urls, 3)
+    thread3.start()
 
 if __name__ == "__main__":
     main()
